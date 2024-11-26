216f2ad8c230f4ca35ef26929cc34c5bfc6ab540
Bao <Bao@gmail.com>
2024-11-26 13:00:50 +0700

    them ReviewResponse

---
+++ src/main/java/org/tour/quanlytour/dtos/response/ReviewResponse.java
@@ -0,0 +1,25 @@
+package org.tour.quanlytour.dtos.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = false)
+public class ReviewResponse {
+    @JsonProperty("title")
+    private String title;
+    private String content;
+    private int rating;
+    @JsonProperty("image_url")
+    private String imageUrl;
+    private String username;
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+}
